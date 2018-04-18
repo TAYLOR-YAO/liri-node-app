6c537daae8fe69f8a2c36ec18156e298388a935b
Your Name <yaotaylor25@gmail.com>
2018-04-17 20:19:19 -0400

    change computer

--- liri.js
+++ liri.js
@@ -1,2 +1,19 @@
-require("dotenv").config();
-var rquireKeysjs = require("keys.js");
+// require("dotenv").config();
+var keys= require("./keys.js");
+var Twitter = require('twitter');
+var omdb = require('omdb');
+var argOne = process.argv[2];
+var argTwo = process.argv[3];
+
+var omdbApiKey = "e443c6de";
+var client = new Twitter(keys.twitter);
+
+
+var userName = { screen_name:argOne + "" + argTwo,
+                count: 2 };
+
+client.get('statuses/user_timeline', userName, function(error, tweets, response) {
+    if(error) throw error;
+    console.log(tweets);  // The favorites. 
+    console.log(response);  // Raw response object. 
+  });
